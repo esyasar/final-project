8255beb1f646baf0f181a11bbd6ec3dbece73705
esyasar <elifsueyasar@gmail.com>
2020-07-11 19:41:28 -0400

    Updates

---
+++ precache-manifest.2631dc44ad4a0971f8990e176e796da7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423daaa346c0d0e13f0d9888ca707f70",
+    "url": "/finalproject/index.html"
+  },
+  {
+    "revision": "b2e9ec30b727b95c23b2",
+    "url": "/finalproject/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "db14237e7ea8e01d9422",
+    "url": "/finalproject/static/css/main.f139927f.chunk.css"
+  },
+  {
+    "revision": "b2e9ec30b727b95c23b2",
+    "url": "/finalproject/static/js/2.65f7a7e4.chunk.js"
+  },
+  {
+    "revision": "db14237e7ea8e01d9422",
+    "url": "/finalproject/static/js/main.c5f870e1.chunk.js"
+  },
+  {
+    "revision": "5cd66c24f798cc8cd677",
+    "url": "/finalproject/static/js/runtime~main.bc5116df.js"
+  },
+  {
+    "revision": "97d7abc2a9809f58cedf56f85fe78127",
+    "url": "/finalproject/static/media/1.97d7abc2.png"
+  },
+  {
+    "revision": "08c236867ba2d800ae01734110b96288",
+    "url": "/finalproject/static/media/2.08c23686.png"
+  },
+  {
+    "revision": "fbe7ffcfe30a3bb72f539939c52ef152",
+    "url": "/finalproject/static/media/3.fbe7ffcf.png"
+  },
+  {
+    "revision": "60020bb78e899612a3c5108e6846ace4",
+    "url": "/finalproject/static/media/4.60020bb7.png"
+  },
+  {
+    "revision": "6e333598db5a7c8087f0d4b22e0be3c3",
+    "url": "/finalproject/static/media/ceo.6e333598.png"
+  },
+  {
+    "revision": "7fcaca766d06bf6cbf96b42409e99112",
+    "url": "/finalproject/static/media/diversity.7fcaca76.svg"
+  },
+  {
+    "revision": "6cbae3228799c5517867d6a0dc8359b7",
+    "url": "/finalproject/static/media/friends.6cbae322.svg"
+  },
+  {
+    "revision": "491b03668d68127d42434bbe502e3e0f",
+    "url": "/finalproject/static/media/grow.491b0366.svg"
+  },
+  {
+    "revision": "04dcae39d13c895cf97aa5241cb9f8b8",
+    "url": "/finalproject/static/media/header-bg.04dcae39.jpg"
+  },
+  {
+    "revision": "29bfb170ff633d5ebba058da76fe73db",
+    "url": "/finalproject/static/media/manager.29bfb170.png"
+  },
+  {
+    "revision": "62fd37ef3203a999f7b9cfdaa4911e86",
+    "url": "/finalproject/static/media/map-image.62fd37ef.png"
+  },
+  {
+    "revision": "08eb7db8932d6d75aea95e0214c74cb7",
+    "url": "/finalproject/static/media/marketing.08eb7db8.png"
+  },
+  {
+    "revision": "9a1be1258006c5e674f75afea84fcf50",
+    "url": "/finalproject/static/media/yalla-cafe.9a1be125.jpg"
+  },
+  {
+    "revision": "6cf3b104ff9ef55f0606845480ffd321",
+    "url": "/finalproject/static/media/yalla-show.6cf3b104.jpg"
+  },
+  {
+    "revision": "3f868a0b9c151d2626cdd4b58b9b758b",
+    "url": "/finalproject/static/media/ylt-conference.3f868a0b.jpeg"
+  }
+]);
